f3021206cbb7e4c272aadb999b82a34326e4ee77
Julliya <julli.babenko@gmail.com>
2019-12-24 22:21:33 +0200

    HW-6 done

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,26 @@
-// Your code goes here
+alert('Please, input 3 numbers (a, b, c) for triangle sides length');
+let a = Number(prompt('Enter A'));
+let b = Number(prompt('Enter B'));
+let c = Number(prompt('Enter C'));
+
+if (Number.isNaN(a) || Number.isNaN(b) || Number.isNaN(c)) {
+	alert('input values should be ONLY numbers');
+} else {
+	if (Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c)) {
+		if (a > 0 && b > 0 && c > 0) {
+			if (a === b && b === c) {
+				console.log('Equilateral triangle');
+			} else if (a === b || b === c || c === a) {
+				console.log('Equilateral triangle');
+			} else if (a + b > c || b + c > a || a + c > b) {
+				console.log('Isosceles triangle');
+			} else {
+				console.log('Triangle doesn’t exist');
+			}
+		} else {
+			alert('A triangle must have 3 sides with a positive definite length');
+		}
+	} else {
+		alert('input values should be ONLY INT numbers');
+	}
+}
