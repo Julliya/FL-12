65be502f4a302a6b76998b61c52e05a72de5670d
Julliya <julli.babenko@gmail.com>
2019-12-27 01:21:31 +0200

    HW-7 in progress

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let start = confirm('Do you want to play a game?');
+let counter = 0;
+
+if (start) {
+	while (counter < 10) {
+		for (let attempts = 3; attempts >= 1; attempts--) {
+			prompt('Choose an integer number in range [0; 8] \n Attempts left:' + attempts);
+		}
+		counter++;
+	}
+} else {
+	alert('You did not become a billionaire, but can');
+}
