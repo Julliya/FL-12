63d6e15e4b9de46b1fc847fa17e57f989c25da76
Julliya <julli.babenko@gmail.com>
2019-12-30 18:14:49 +0200

    HW-8 dome task7

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -1,10 +1,20 @@
 function isLeapYear(date) {
-	let input = date;
-	return input instanceof Date;
+	let d = new Date(date);
+	if (!isNaN(d.getTime())) {
+		let year = d.getFullYear();
+		if (new Date(year, 1, 29).getDate() === 29) {
+			return year + ' is a leap year';
+		} else {
+			return year + ' is not a leap year';
+		}
+	} else {
+		return 'Invalid Date';
+	}
 }
+
 console.log(isLeapYear('2020-01-01 00:00:00')); // =>  ‘2020 is a leap year’
-isLeapYear('2020-01-01 00:00:00777'); // =>  ‘Invalid Date’
-isLeapYear('2021-01-15 13:00:00'); // =>  ‘2021 is not a leap year’
-isLeapYear('2200-01-15 13:00:00'); // =>  ‘2200 is not a leap year’
+console.log(isLeapYear('2020-01-01 00:00:00777')); // =>  ‘Invalid Date’
+console.log(isLeapYear('2021-01-15 13:00:00')); // =>  ‘2021 is not a leap year’
+console.log(isLeapYear('2200-01-15 13:00:00')); // =>  ‘2200 is not a leap year’
 console.log(isLeapYear(1213131313135465656654564646542132132131)); // =>  ‘Invalid Date’
 console.log(isLeapYear(1213131313));
