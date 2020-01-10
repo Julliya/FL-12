f1332e45b14b11a63157406a18efcc20ae953f52
Julliya <julli.babenko@gmail.com>
2020-01-10 22:23:16 +0200

    HW-9 1,2 task done

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+function convert(...args) {
+	for (let i = 0; i < args.length; i++) {
+		if (typeof args[i] === 'string') {
+			args[i] = +args[i];
+		} else {
+			args[i] = '' + args[i];
+		}
+	}
+	console.log(...args);
+}
+convert('1', 2, 3, '4'); // [1, '2', '3', 4]
+
+function executeforEach(arr, callback) {
+	for (let i = 0; i < arr.length; i++) {
+		callback(arr[i]);
+	}
+}
+executeforEach([ 1, 2, 3 ], function(el) {
+	console.log(el * 2);
+});
